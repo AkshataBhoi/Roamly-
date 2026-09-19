dc51003c794c73b4d648ffcead80cdf47ab818f1
Akshata Bhoi <akshatabbhoi@gmail.com>
2026-09-19 11:39:07 +0530

    chore: untrack and ignore dist directory

--- backend/dist/routes/location.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const location_controller_1 = require("../controllers/location.controller");
-const router = (0, express_1.Router)();
-router.get('/geocode', location_controller_1.geocode);
-router.get('/reverse', location_controller_1.reverse);
-exports.default = router;
